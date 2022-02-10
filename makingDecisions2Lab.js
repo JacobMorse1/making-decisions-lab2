4105f6111c11fa0c247318fa0558da6af1a60c46
Jacob Morse <jacobsmorse@gmail.com>
2022-02-10 14:45:12 -0700

    Problem Five Done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -51,13 +51,13 @@ let evensArr = []
 */
 
 //Code Here
-for (let i = 0; i < nums.length; i++) {
-  if(nums[i] % 2 === 0){
-  evensArr.push(nums[i])
-} 
-}
-console.log("evensArr", evensArr)
-console.log("nums", nums)
+// for (let i = 0; i < nums.length; i++) {
+//   if(nums[i] % 2 === 0){
+//   evensArr.push(nums[i])
+// } 
+// }
+// console.log("evensArr", evensArr)
+// console.log("nums", nums)
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -65,11 +65,22 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above,
+   console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and
+    anything below 60 should console log an 'F'.
 */
 
-//Code Here
-
+if (score >= 90){
+  console.log("A")
+} else if (score >= 80 && score <= 89){
+  console.log("B")
+} else if (score >= 70 && score <= 79){
+  console.log("C")
+} else if (score >= 60 && score <= 69) {
+  console.log("D")
+}else {
+  console.log("F")
+}
 
 ////////// Intermediate Problems //////////
 
